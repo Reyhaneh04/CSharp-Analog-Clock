5b32c3968a4bcdcb54ead1a962aa6190b4638dc8
Reyhaneh04 <reyhanehdehghanan@gmail.com>
2026-09-16 14:43:07 +0330

    Add C# analog clock project

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.ovclock = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.lnHour = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lnMinute = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lnSec = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lnSec,
+            this.lnMinute,
+            this.lnHour,
+            this.ovclock});
+            this.shapeContainer1.Size = new System.Drawing.Size(499, 442);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // ovclock
+            // 
+            this.ovclock.Location = new System.Drawing.Point(8, 10);
+            this.ovclock.Name = "ovclock";
+            this.ovclock.Size = new System.Drawing.Size(353, 339);
+            this.ovclock.Click += new System.EventHandler(this.ovclock_Click);
+            // 
+            // lnHour
+            // 
+            this.lnHour.Name = "lnHour";
+            this.lnHour.X1 = 186;
+            this.lnHour.X2 = 182;
+            this.lnHour.Y1 = 53;
+            this.lnHour.Y2 = 180;
+            this.lnHour.Click += new System.EventHandler(this.lnHour_Click);
+            // 
+            // lnMinute
+            // 
+            this.lnMinute.Name = "lnMinute";
+            this.lnMinute.X1 = 147;
+            this.lnMinute.X2 = 181;
+            this.lnMinute.Y1 = 85;
+            this.lnMinute.Y2 = 181;
+            // 
+            // lnSec
+            // 
+            this.lnSec.Name = "lnSec";
+            this.lnSec.X1 = 166;
+            this.lnSec.X2 = 179;
+            this.lnSec.Y1 = 102;
+            this.lnSec.Y2 = 180;
+            this.lnSec.Click += new System.EventHandler(this.lnSec_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_1.Properties.Resources._1س;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(499, 442);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lnSec;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lnMinute;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lnHour;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovclock;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        int x0, y0;
+        int rH, rM, rS;
+        double rRatio;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x0 = ovclock.Left + ovclock.Width / 2;
+            y0 = ovclock.Top + ovclock.Height / 2;
+            rS = ovclock.Width / 2 - 60;
+            rM = rS + 10;
+            rH = rM + 10;
+            lnHour.BorderWidth = 3;
+            lnMinute.BorderWidth = 2;
+            lnSec.BorderColor = Color.Red;
+            // تنظیم ابتدای خطوط به مبدا مختصات
+            lnHour.X1 = lnMinute.X1 = lnSec.X1 = x0;
+            lnHour.Y1 = lnMinute.Y1 = lnSec.Y1 = y0;
+            // تنظیم بلندی خطوط
+            lnHour.X2 = rH; lnMinute.X2 = rM; lnSec.X2 = rS;
+            lnHour.Y2 = lnMinute.Y2 = lnSec.Y2 = y0;
+            rRatio = Math.PI / 180;
+            timer1_Tick(sender, e);
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now.ToLocalTime();
+            int H = dt.Hour % 12, M = dt.Minute, S = dt.Second;
+            double dH = ((H - 3) * 30 + M * .5) * rRatio;
+            double dM = (M - 15) * 6 * rRatio, dS = (S - 15) * 6 * rRatio;
+            lnSec.X2 = (int)(x0 + rS * Math.Cos(dS));
+            lnSec.Y2 = (int)(y0 + rS * Math.Sin(dS));
+            lnMinute.X2 = (int)(x0 + rM * Math.Cos(dM));
+            lnMinute.Y2 = (int)(y0 + rM * Math.Sin(dM));
+            lnHour.X2 = (int)(x0 + rH * Math.Cos(dH));
+            lnHour.Y2 = (int)(y0 + rH * Math.Sin(dH));
+            this.Text = dt.ToString("hh:mm:ss");
+        }
+
+        private void ovclock_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lnHour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lnSec_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
